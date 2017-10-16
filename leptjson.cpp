f87a83f8b3cd59b04a61c5744963c9c5f5e85471
sixdaycoder <sixdaycoder@gmail.com>
2017-10-16 20:01:00 +0800

    重构

--- leptjson.cpp
+++ leptjson.cpp
@@ -1,137 +1,71 @@
+#ifdef _WINDOWS
+#define _CRTDBG_MAP_ALLOC
+#include <crtdbg.h>
+#endif
 #include "leptjson.h"
+#include <assert.h>  /* assert() */
+#include <errno.h>   /* errno, ERANGE */
+#include <math.h>    /* HUGE_VAL */
+#include <stdlib.h>  /* NULL, malloc(), realloc(), free(), strtod() */
+#include <string.h>  /* memcpy() */
 
-double LeptGetNumber(const LeptValue & v)
-{
-	assert(v.type == LeptType::LEPT_NUMBER);
-	return v.u.number;
-}
+#ifndef LEPT_PARSE_STACK_INIT_SIZE
+#define LEPT_PARSE_STACK_INIT_SIZE 256
+#endif
 
-ParseState LeptParse(const char* json, LeptValue& v)
-{
-	assert(json != nullptr);
+#define EXPECT(c, ch)       do { assert(*c->json == (ch)); c->json++; } while(0)
+#define ISDIGIT(ch)         ((ch) >= '0' && (ch) <= '9')
+#define ISDIGIT1TO9(ch)     ((ch) >= '1' && (ch) <= '9')
+#define PUTC(c, ch)         do { *(char*)LeptContextPush(c, sizeof(char)) = (ch); } while(0)
 
-	v.type = LeptType::LEPT_NULL;
-	LeptContext context;
-	context.json = json;
+typedef struct {
 
-	//去掉json字符串开始的空白符
-	LeptParseWhitespace(context);
+	const char* json;
+	char* stack;
+	size_t size, top;
 
-	ParseState ret;
-	ret = LeptParseValue(context, v);
+}LeptContext;
 
-	if (ret == ParseState::LEPT_PARSE_SUCCESS) {
-		//去掉解析值后的空格
-		LeptParseWhitespace(context);
-		//如果去掉空格后字符串没有结束
-		if (*context.json != '\0') {
-
-			v.type = LeptType::LEPT_NULL;
-			return ParseState::LEPT_PARSE_ROOT_NOT_SINGULAR;
-
-		}
-		else
-			return ParseState::LEPT_PARSE_SUCCESS;
+static void* LeptContextPush(LeptContext* c, size_t size) {
+	void* ret;
+	assert(size > 0);
+	if (c->top + size >= c->size) {
+		if (c->size == 0)
+			c->size = LEPT_PARSE_STACK_INIT_SIZE;
+		while (c->top + size >= c->size)
+			c->size += c->size >> 1;  /* c->size * 1.5 */
+		c->stack = (char*)realloc(c->stack, c->size);
 	}
-
+	ret = c->stack + c->top;
+	c->top += size;
 	return ret;
 }
 
-void LeptParseWhitespace(LeptContext & context)
-{
-	const char* p = context.json;
+static void* LeptContextPop(LeptContext* c, size_t size) {
+	assert(c->top >= size);
+	return c->stack + (c->top -= size);
+}
 
-	while (*p == ' ' || *p == '\t' || *p == '\r' || *p == '\n')
+static void LeptParseWhitespace(LeptContext* c) {
+	const char *p = c->json;
+	while (*p == ' ' || *p == '\t' || *p == '\n' || *p == '\r')
 		p++;
-
-	context.json = p;
+	c->json = p;
 }
 
-ParseState LeptParseValue(LeptContext& context, LeptValue& v)
-{
-	switch (*context.json) {
-
-	case 'n':  return LeptParseNull(context, v);
-
-	case 't':  return LeptParseTrue(context, v);
-
-	case 'f':  return LeptParseFalse(context, v);
-
-	default:    return LeptParseNumber(context, v);
-
-	case '\0':  return ParseState::LEPT_PARSE_EXPECT_VALUE;
-
-	}
-
+static int LeptParseLiteral(LeptContext* c, LeptValue* v, const char* literal, LeptType type) {
+	size_t i;
+	EXPECT(c, literal[0]);
+	for (i = 0; literal[i + 1]; i++)
+		if (c->json[i] != literal[i + 1])
+			return LeptParse_INVALID_VALUE;
+	c->json += i;
+	v->type = type;
+	return LeptParseSuccess;
 }
 
-ParseState LeptParseNull(LeptContext& context, LeptValue& v)
-{
-	assert(*context.json == 'n');
-
-	context.json++;
-
-	if (context.json[0] != 'u' || context.json[1] != 'l' || context.json[2] != 'l') {
-
-		return ParseState::LEPT_PARSE_INVALID_VALUE;
-
-	}
-	else {
-
-		context.json += 3;
-		v.type = LeptType::LEPT_NULL;
-
-		return ParseState::LEPT_PARSE_SUCCESS;
-	}
-
-}
-
-ParseState LeptParseTrue(LeptContext & context, LeptValue & v)
-{
-	assert(*context.json == 't');
-
-	context.json++;
-
-	if (context.json[0] != 'r' || context.json[1] != 'u' || context.json[2] != 'e') {
-
-		return ParseState::LEPT_PARSE_INVALID_VALUE;
-
-	}
-	else {
-
-		context.json += 3;
-		v.type = LeptType::LEPT_TRUE;
-
-		return ParseState::LEPT_PARSE_SUCCESS;
-
-	}
-
-}
-
-ParseState LeptParseFalse(LeptContext & context, LeptValue & v)
-{
-	assert(*context.json == 'f');
-
-	context.json++;
-
-	if (context.json[0] != 'a' || context.json[1] != 'l' || context.json[2] != 's' || context.json[3] != 'e') {
-
-		return ParseState::LEPT_PARSE_INVALID_VALUE;
-
-	}
-	else {
-
-		context.json += 4;
-		v.type = LeptType::LEPT_FALSE;
-
-		return ParseState::LEPT_PARSE_SUCCESS;
-
-	}
-}
-
-ParseState LeptParseNumber(LeptContext & context, LeptValue & v)
-{
-	const char* json = context.json;
+static int LeptParseNumber(LeptContext* c, LeptValue* v) {
+	const char* p = c->json;
 
 	/*
 	json数字的构成
@@ -144,58 +78,44 @@ ParseState LeptParseNumber(LeptContext & context, LeptValue & v)
 	*/
 
 	/*(0)校验负号*/
-	if (*json == '-')
-		json++;
+	if (*p == '-') p++;
 
 	/*(1)校验int, 两种情况,*/
-	if (*json == '0') {
-		json++;
-	}
+	if (*p == '0') p++;
 	else {
-
-		if (!ISDIGIT1TO9(*json)) return ParseState::LEPT_PARSE_INVALID_VALUE;
-
-		for (json++; ISDIGIT(*json); json++);
-
+		if (!ISDIGIT1TO9(*p)) return LeptParse_INVALID_VALUE;
+		for (p++; ISDIGIT(*p); p++);
 	}
 
 	/*(2)校验小数点*/
-	if (*json == '.') {
+	if (*p == '.') {
 		//小数点后至少一位是数字
-		json++;
-
-		if (!ISDIGIT(*json)) return ParseState::LEPT_PARSE_INVALID_VALUE;
-
-		for (json++; ISDIGIT(*json); json++);
-
+		p++;
+		if (!ISDIGIT(*p)) return LeptParse_INVALID_VALUE;
+		for (p++; ISDIGIT(*p); p++);
 	}
 
 	/*(3)校验指数部分*/
-	if (*json == 'e' || *json == 'E') {
-		json++;
+	if (*p == 'e' || *p == 'E') {
+		p++;
+
 		//跳过'e'或者'E'后,后面可以有正负号,检验
 		/*(3.1)校验跳过E或者e后,是否有正负号*/
-		if (*json == '+' || *json == '-') json++;
+		if (*p == '+' || *p == '-') p++;
+		if (!ISDIGIT(*p)) return LeptParse_INVALID_VALUE;
 
 		/*(3.2)校验跳过了正负号后后面是否都为数字*/
-		for (json++; ISDIGIT(*json); json++);
+		for (p++; ISDIGIT(*p); p++);
 	}
 
 	//经过上述处理,json指向第一个不为数字的字符
-
 	errno = 0;
-	double number = strtold(context.json, NULL);
-
-	if (errno == ERANGE && (number == HUGE_VAL || number == -HUGE_VAL))
-		return ParseState::LEPT_PARSE_NUMBER_TOO_BIG;
-
-
-	v.u.number = number;
-	v.type = LeptType::LEPT_NUMBER;
-	context.json = json;
-
-	return ParseState::LEPT_PARSE_SUCCESS;
-
+	v->u.n = strtod(c->json, NULL);
+	if (errno == ERANGE && (v->u.n == HUGE_VAL || v->u.n == -HUGE_VAL))
+		return LeptParseNumber_TOO_BIG;
+	v->type = LEPT_NUMBER;
+	c->json = p;
+	return LeptParseSuccess;
 
 	/*
 	errno :
@@ -222,84 +142,144 @@ ParseState LeptParseNumber(LeptContext & context, LeptValue & v)
 	*/
 }
 
-ParseState LeptParseString(LeptContext & context, LeptValue& v)
-{
-	size_t head = context.top, len;
-	const char* p = context.json;
-
-	while (true) {
-
-		char c = *p++;
-		switch (c) {
-			case '\"':
-				len = context.top - head;
-				LeptSetString((const char*)LeptContextPop(context, len), len, v);
-				context.json = p;
-				return ParseState::LEPT_PARSE_SUCCESS;
+static int LeptParseString(LeptContext* c, LeptValue* v) {
+	size_t head = c->top, len;
+	const char* p;
+	EXPECT(c, '\"');
+	p = c->json;
+	while(true) {
+		char ch = *p++;
+		switch (ch) {
+		case '\"':
+			len = c->top - head;
+			LeptSetString(v, (const char*)LeptContextPop(c, len), len);
+			c->json = p;
+			return LeptParseSuccess;
+		case '\\':
+			switch (*p++) {
+			case '\"': PUTC(c, '\"'); break;
+			case '\\': PUTC(c, '\\'); break;
+			case '/':  PUTC(c, '/'); break;
+			case 'b':  PUTC(c, '\b'); break;
+			case 'f':  PUTC(c, '\f'); break;
+			case 'n':  PUTC(c, '\n'); break;
+			case 'r':  PUTC(c, '\r'); break;
+			case 't':  PUTC(c, '\t'); break;
+			default:
+				c->top = head;
+				return LeptParse_INVALID_STRING_ESCAPE;
+			}
 			break;
-
-			case '\0':
-				context.top = head;
+		case '\0':
+			c->top = head;
+			return LeptParse_MISS_QUOTATION_MARK;
+		default:
+			if ((unsigned char)ch < 0x20) {
+				c->top = head;
+				return LeptParse_INVALID_STRING_CHAR;
+			}
+			PUTC(c, ch);
 		}
-
 	}
 }
 
-void LeptSetString(const char * s, size_t len, LeptValue & v)
-{
-	assert((s != NULL) || (len != 0));
-
-	v.u.str.s = (char*)malloc(len + 1);
-
-	memcpy(v.u.str.s, s, len);
-
-	v.u.str.s[len] = '\0';
-	v.u.str.len = len;
-
-	v.type = LeptType::LEPT_STRING;
-
+static int LeptParseValue(LeptContext* c, LeptValue* v) {
+	switch (*c->json) {
+		case 't':  return LeptParseLiteral(c, v, "true", LEPT_TRUE);
+		case 'f':  return LeptParseLiteral(c, v, "false", LEPT_FALSE);
+		case 'n':  return LeptParseLiteral(c, v, "null", LEPT_NULL);
+		default:   return LeptParseNumber(c, v);
+		case '"':  return LeptParseString(c, v);
+		case '\0': return LeptParse_EXPECT_VALUE;
+	}
 }
 
-void LeptFreeString(LeptValue & v)
+void LeptInit(LeptValue * v)
 {
-	assert(v.u.str.s != NULL);
-
-	free(v.u.str.s);
-	v.u.str.s = NULL;
+	assert(v != NULL);
+	v->type = LEPT_NULL;
 }
 
-void * LeptContextPush(LeptContext & c, size_t size)
-{
-	assert(size > 0);
+int LeptParse(LeptValue* v, const char* json) {
 
-	void* ret;
-	
-	//超过了当前栈的size,扩充栈
-	if (c.top + size >= c.size) {
+	LeptContext c;
+	int ret;
+	assert(v != NULL);
+	c.json = json;
+	c.stack = NULL;
+	c.size = c.top = 0;
+	LeptInit(v);
 
-		//还未给栈分配内存
-		if (c.size == 0) {
-			c.size = LEPT_PARSE_STACK_INIT_SIZE;
+	//去掉json字符串开始的空白符
+	LeptParseWhitespace(&c);
+	if ((ret = LeptParseValue(&c, v)) == LeptParseSuccess) {
+		//去掉解析值后的空格
+		LeptParseWhitespace(&c);
+		//如果去掉空格后字符串没有结束
+		if (*c.json != '\0') {
+			v->type = LEPT_NULL;
+			ret = LeptParse_ROOT_NOT_SINGULAR;
 		}
-
-		while (c.top + size >= c.size) {
-			//1.5倍分配
-			c.size += c.size >> 1;
-		}
-
-		c.stack = (char*)realloc(c.stack, c.size);
-
 	}
 
-	ret = c.stack + c.top;
-	c.top += size;
-
+	//添加该断言保证free的逻辑正确性
+	assert(c.top == 0);
+	free(c.stack);
 	return ret;
 
 }
 
-void * LeptContextPop(LeptContext & c, size_t size)
-{
-	assert(c.top >= size && size > 0);
-	return c.stack + (c.top -= size);
+void LeptFree(LeptValue* v) {
+
+	assert(v != NULL);
+	if (v->type == LEPT_STRING)
+		free(v->u.s.s);
+	v->type = LEPT_NULL;
+
+}
+
+LeptType LeptGetType(const LeptValue* v) {
+	assert(v != NULL);
+	return v->type;
+}
+
+int LeptGetBoolean(const LeptValue* v) {
+	assert(v != NULL && (v->type == LEPT_TRUE || v->type == LEPT_FALSE));
+	return v->type == LEPT_TRUE;
+}
+
+void LeptSetBoolean(LeptValue* v, int b) {
+	LeptFree(v);
+	v->type = b ? LEPT_TRUE : LEPT_FALSE;
+}
+
+double LeptGetNumber(const LeptValue* v) {
+	assert(v != NULL && v->type == LEPT_NUMBER);
+	return v->u.n;
+}
+
+void LeptSetNumber(LeptValue* v, double n) {
+	LeptFree(v);
+	v->u.n = n;
+	v->type = LEPT_NUMBER;
+}
+
+const char* LeptGetString(const LeptValue* v) {
+	assert(v != NULL && v->type == LEPT_STRING);
+	return v->u.s.s;
+}
+
+size_t LeptGetStringLength(const LeptValue* v) {
+	assert(v != NULL && v->type == LEPT_STRING);
+	return v->u.s.len;
+}
+
+void LeptSetString(LeptValue* v, const char* s, size_t len) {
+	assert(v != NULL && (s != NULL || len == 0));
+	LeptFree(v);
+	v->u.s.s = (char*)malloc(len + 1);
+	memcpy(v->u.s.s, s, len);
+	v->u.s.s[len] = '\0';
+	v->u.s.len = len;
+	v->type = LEPT_STRING;
 }
--- leptjson.h
+++ leptjson.h
@@ -1,142 +1,67 @@
+#ifndef LEPTJSON_H__
+#define LEPTJSON_H__
 
-#ifndef  SIXDAY_LEPTJSON_H
-#define  SIXDAY_LEPTJSON_H
+#include <stdlib.h> /* size_t */
 
-#include <cstdio>
-#include <cstdlib>
-#include <cassert>
-#include <cerrno>
-#include <cstring>
+typedef enum { LEPT_NULL, LEPT_FALSE, LEPT_TRUE, LEPT_NUMBER, LEPT_STRING, LEPT_ARRAY, LEPT_OBJECT } LeptType;
 
-
-#define ISDIGIT(ch)      ( (ch) >= '0' && (ch) <= '9')
-#define ISDIGIT1TO9(ch)  ( (ch) >= '1' && (ch) <= '9')
-
-
-#ifndef LEPT_PARSE_STACK_INIT_SIZE
-#define LEPT_PARSE_STACK_INIT_SIZE 256
-#endif // !1
-
-
-enum LeptType {
-
-	LEPT_NULL,
-	LEPT_FALSE,
-	LEPT_TRUE,
-	LEPT_NUMBER,
-	LEPT_STRING,
-	LEPT_ARRAY,
-	LEPT_STRING,
-
-};
-
-enum ParseState {
-
-	LEPT_PARSE_SUCCESS,
-	LEPT_PARSE_EXPECT_VALUE,       //json只含有空白符
-	LEPT_PARSE_ROOT_NOT_SINGULAR, //若一个值在空白之后还有其他的值,意味着根节点不唯一
-	LEPT_PARSE_NUMBER_TOO_BIG,    //数字越上届
-	LEPT_PARSE_INVALID_VALUE     //除上述两种错误之外的错误
-
-};
-
-struct LeptValue {
+typedef struct {
 
 	union {
-
-		struct{
-			char* s;
-			size_t len;
-		}str;
-
-		double number;
-
+		struct { char* s; size_t len; }s;  /* string: null-terminated string, string length */
+		double n;                          /* number */
 	}u;
-	
+
 	LeptType type;
 
+}LeptValue;
+
+enum {
+
+	LeptParseSuccess = 0,
+	LeptParse_EXPECT_VALUE,
+	LeptParse_INVALID_VALUE,
+	LeptParse_ROOT_NOT_SINGULAR,
+	LeptParseNumber_TOO_BIG,
+	LeptParse_MISS_QUOTATION_MARK,
+	LeptParse_INVALID_STRING_ESCAPE,
+	LeptParse_INVALID_STRING_CHAR
+
 };
 
 
-struct LeptContext {
+LeptType 
+LeptGetType(const LeptValue* v);
 
-	const char* json;
+void	 
+LeptInit(LeptValue* v);
 
-	char* stack;
-	size_t size, top;
+int		 
+LeptParse(LeptValue* v, const char* json);
 
-};
-
-/// 如果类型为数字,获取LeptValue的number
-double LeptGetNumber(const LeptValue& v);
-
-
-/// 构造解析树
-/// json(in) : json文本
-/// v(out)   : 解析对象
-ParseState LeptParse(const char* json, LeptValue& v);
+void	 
+LeptFree(LeptValue* v);
 
 
-/// 跳过空白符
-/// context(in out) : 解析器上下文
-void LeptParseWhitespace(LeptContext& context);
+int    
+LeptGetBoolean(const LeptValue* v);
 
+void	
+LeptSetBoolean(LeptValue* v, int b);
 
-/// 解析字面值
-/// context(in out) : 解析器上下文
-/// v(out)      : 解析对象
-ParseState LeptParseValue(LeptContext& context, LeptValue& v);
+double		
+LeptGetNumber(const LeptValue* v);
 
+void		
+LeptSetNumber(LeptValue* v, double n);
 
-/// 解析NULL
-/// context(in out) : 解析器上下文
-/// v(out)      : 解析对象
-ParseState LeptParseNull(LeptContext& context, LeptValue& v);
+const char* 
+LeptGetString(const LeptValue* v);
 
+size_t 
+LeptGetStringLength(const LeptValue* v);
 
-/// 解析true值
-/// context(in out) : 解析器上下文
-/// v(out)      : 解析对象
-ParseState LeptParseTrue(LeptContext& context, LeptValue& v);
+void 
+LeptSetString(LeptValue* v, const char* s, size_t len);
 
-
-/// 解析false值
-/// context(in out) : 解析器上下文
-/// v(out)      : 解析对象
-ParseState LeptParseFalse(LeptContext& context, LeptValue& v);
-
-/// 解析数字值
-/// context(in out) : 解析器上下文
-/// v(out)  : 解析对象
-ParseState LeptParseNumber(LeptContext& context, LeptValue& v);
-
-
-/// 解析字符串
-/// context(in out) : 解析器上下文
-/// v(out)  : 解析对象
-ParseState LeptParseString(LeptContext& context, LeptValue& v);
-
-/// 动态分配字符串
-/// s(in)  ： 字符串
-/// len(in)： 字符串的长度
-/// v(out) ： 解析对象
-void LeptSetString(const char* s, size_t len, LeptValue& v);
-
-
-/// 回收分配的字符串
-/// v(in out) 存储字符串
-void LeptFreeString(LeptValue& v);
-
-
-/// 以context建立字符串解析的缓冲区,临时存储字符串
-/// c(in out) : 解析器上下文
-/// size(in)  : 要压栈的字符串的长度 
-void* LeptContextPush(LeptContext& c, size_t size);
-
-
-/// 以context建立字符串解析的缓冲区,临时存储字符串
-/// c(in out) : 解析器上下文
-/// size(in)  : 要弹出的字符串的长度 
-void* LeptContextPop(LeptContext& c, size_t size);
-#endif // ! SIXDAY_LEPTJSON_H
-
+#endif /* LEPTJSON_H__ */
